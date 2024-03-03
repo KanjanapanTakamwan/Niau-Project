ecddd6eb79ff8ffa7b6a984aee4d948d8e3b31ea
Dream65010286 <154393230+Dream65010286@users.noreply.github.com>
2024-03-03 20:22:49 +0700

    try on

--- src/components/NavigatorBar.js
+++ src/components/NavigatorBar.js
@@ -5,19 +5,19 @@ export default function Navigator() {
         <div className={styles.navbar}>
             <div className={styles.navbarContainer}>
                 <div className={styles.navbox}>
-                    <img src="/home.svg" />
+                <Link href="/"><img src="/home.svg" /></Link>
                     <Link href="/"> Home</Link></div>
 
                 <div className={styles.navbox}>
-                    <img src="/recom.svg" />
+                <Link href="/recommended"><img src="/recom.svg" /></Link>
                     <Link href="/recommended"> Recommended</Link></div>
 
                 <div className={styles.navbox}>
-                    <img src="/search.svg" />
+                <Link href="/search" ><img src="/search.svg" /></Link>
                     <Link href="/search" > Search</Link></div>
 
                 <div className={styles.navbox}>
-                    <img src="/user.svg" />
+                <Link href="/profile"><img src="/user.svg" /></Link>
                     <Link href="/profile">Profile</Link></div>
 
             </div>
